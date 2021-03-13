dfc53c83b1f4d12c3711e092e8316c03be41d9ee
Rafael <rafael@i2mobile.com.br>
2021-03-13 11:12:53 -0300

    Agora tem duas classas que são filhas da class abstrata Funcionario

---
+++ api-collections/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioAnalista.kt
@@ -0,0 +1,10 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Analista
+import one.digitalinnovation.digionebank.Funcionario
+
+fun main(){
+    val a1 = Analista("Rafael", "07180607410", 2000.00)
+     ImprimeRelatorioFuncionario.imprime(a1)
+
+}
---
+++ api-collections/src/main/kotlin/one/digitalinnovation/digionebank/testes/TesteFuncionarioGerente.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digionebank.testes
+import one.digitalinnovation.digionebank.Gerente
+
+fun main(){
+    val g1 = Gerente("Tatiana", "05766906439", 5000.00)
+     ImprimeRelatorioFuncionario.imprime(g1)
+
+}
